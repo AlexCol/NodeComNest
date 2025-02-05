e0cee29cb6b0341dbf781cc8db0e0afe7f44abf9
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2025-02-05 13:13:51 -0300

    .

---
+++ 04_crud/src/index.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import 'dotenv/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log('PORT:', process.env.PORT); // Verifique se imprime o valor correto
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
