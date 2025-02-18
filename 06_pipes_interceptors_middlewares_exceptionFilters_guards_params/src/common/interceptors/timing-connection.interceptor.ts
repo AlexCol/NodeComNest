603b38303b6c6ba06911c26243d924335a7f6202
AlexCol <eu_axil@yahoo.com.br>
2025-02-18 13:38:12 -0300

    Interceptor

--- 06_pipes_interceptors_middlewares_exceptionFilters_guards_params/src/common/interceptors/timing-connection.interceptor.ts
+++ 06_pipes_interceptors_middlewares_exceptionFilters_guards_params/src/common/interceptors/timing-connection.interceptor.ts
@@ -1,16 +1,22 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
-import { Observable, tap } from "rxjs";
+import { finalize, Observable, tap } from "rxjs";
 
 export class TimingConnectionInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler<any>): Promise<Observable<any>> {
     console.log('TimingConnectionInterceptor chamado.');
 
+    const startTime = Date.now();
     await new Promise(resolve => setTimeout(resolve, 3000));
 
     return next.handle().pipe(
+      //tap só é executado se o next.handle() for executado com sucesso (exceções não tratadas fazo tap não ser chamado)
+      //finalize é chamado sempre, independente se o next.handle() foi executado com sucesso ou não
       tap(
-        () => {
-          console.log(`TimingConnectionInterceptor executado Depois de 3 segundos`);
+        (retornoDoNext) => {
+          console.log(retornoDoNext);
+          const finalTime = Date.now();
+          const elapsedTime = finalTime - startTime;
+          console.log(`Tempo de execução: ${elapsedTime}ms`);
         }
       )
     );
